65e8dacb93525b142f4ce211d0d91d5e04636585
Talap-creator <talap.kienzhiebaiev@mail.ru>
2024-05-21 01:45:33 +0500

    settgins udates155655asdasdsad

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,19 +16,25 @@ const Navbar = () => {
   return (
     <nav className="flex justify-between items-center p-4 bg-gray-900 shadow-2xl px-8">
       <div>
-        <h1 className="text-3xl font-bold neon-text shadow-2xl"><Link href='/'>TechnoTreasures</Link></h1>
+        <h1 className="text-3xl font-bold neon-text shadow-2xl">
+          <Link href='/'><a>Tech Marketplace</a></Link>
+        </h1>
       </div>
       <div className="flex items-center">
         {!isSignedIn && (
-          <a href="/sign-in" className="flex items-center mr-6 text-gray-300 hover:text-blue-400 transition duration-300 shadow-2xl">
-            <AiOutlineLogin size="1.5em" />
-            <span className="ml-2">Sign In</span>
-          </a>
+          <Link href="/sign-in">
+            <a className="flex items-center mr-6 text-gray-300 hover:text-blue-400 transition duration-300 shadow-2xl">
+              <AiOutlineLogin size="1.5em" />
+              <span className="ml-2">Sign In</span>
+            </a>
+          </Link>
         )}
-        <a href="/cart" className="flex items-center text-gray-300 hover:text-blue-400 transition duration-300 shadow-2xl">
-          <AiOutlineShoppingCart size="1.5em" />
-          <span className="ml-2">Cart</span>
-        </a>
+        <Link href="/cart">
+          <a className="flex items-center text-gray-300 hover:text-blue-400 transition duration-300 shadow-2xl">
+            <AiOutlineShoppingCart size="1.5em" />
+            <span className="ml-2">Cart</span>
+          </a>
+        </Link>
       </div>
     </nav>
   );
